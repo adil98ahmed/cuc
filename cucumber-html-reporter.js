ce1c7ee38a7ec1a85dbcf4f17ecff67ade79b7ff
adel ahmed <aadil0384@gmail.com>
2024-08-12 16:20:10 +0300

    ww

--- cucumber-html-reporter.js
+++ cucumber-html-reporter.js
@@ -1,7 +1,7 @@
 const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
-  jsonDir: "./cypress/cucumber-json/",
+  jsonDir: "./cypress/cucumber-json/userListing.cucumber.json",
   reportPath: "./target/rteports/",
   metadata: {
     browser: {
